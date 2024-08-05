c1ae1b1f3dc3b8dad7af0a22df2d26f283d0c5e2
Frozen-369 <frozenboyarp@gmail.com>
2024-08-05 09:41:40 +0545

    Initialization

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button>Click me</Button>
+      </header>
+      <div>
+        <p className='bg-red-500 text-white'>Hello World</p>
+      </div>
+      </div>
+  );
+}
+
+export default App;
